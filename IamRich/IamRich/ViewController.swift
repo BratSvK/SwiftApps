497e78602d47e5c43de05b392f3a93b8982ec33d
BratSvK <martingajdos.zs@gmail.com>
2021-10-26 19:49:48 +0200

    edit new project IAMRICH

---
+++ IamRich/IamRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IamRich
+//
+//  Created by Martin Gajdos on 26/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
