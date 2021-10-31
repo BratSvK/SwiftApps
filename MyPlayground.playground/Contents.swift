4556515ff20feca5eff6fece16adc67092e00ba6
BratSvK <martingajdos.zs@gmail.com>
2021-10-31 20:36:02 +0100

    edit new proejcts

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+
+var a = 5;
+var b = 8;
+
+var c = a;
+a = b;
+b = c;
+
+print("The value of a is \(a)");
+print("The value of b is \(b)");
+
+// arrays
+
+let numbers = [45, 50, 55, 60];
+
+let computedNumbers = [numbers[0]*numbers[1], numbers[1]*numbers[2], numbers[2]*numbers[3], numbers[3]*numbers[0]];
+
+
+
