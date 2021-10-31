9bf5b81e96aa916d8e90db981af184435782ba9b
BratSvK <martingajdos.zs@gmail.com>
2021-10-31 21:21:46 +0100

    edit new project

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -15,5 +15,12 @@ let numbers = [45, 50, 55, 60];
 
 let computedNumbers = [numbers[0]*numbers[1], numbers[1]*numbers[2], numbers[2]*numbers[3], numbers[3]*numbers[0]];
 
+let ab = Int.random(in: 0..<26);
+print(ab);
 
+let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+let password = alphabet[Int.random(in: 0..<26)] + alphabet[Int.random(in: 0..<26)] + alphabet[Int.random(in: 0..<26)] + alphabet[Int.random(in: 0..<26)] + alphabet[Int.random(in: 0..<26)] + alphabet[Int.random(in: 0..<26)];
+    
+print(password)
 
