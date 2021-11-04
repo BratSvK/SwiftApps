4500fc749bf07b4e52dab5af63f97b2010ae6b32
BratSvK <martingajdos.zs@gmail.com>
2021-11-04 20:04:08 +0100

    edit new project

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,26 +1,49 @@
+//import Darwin
+//
+//var a = 5;
+//var b = 8;
+//
+//var c = a;
+//a = b;
+//b = c;
+//
+//print("The value of a is \(a)");
+//print("The value of b is \(b)");
+//
 
-var a = 5;
-var b = 8;
+import Darwin
+//// arrays
+//
+//let numbers = [45, 50, 55, 60];
+//
+//let computedNumbers = [numbers[0]*numbers[1], numbers[1]*numbers[2], numbers[2]*numbers[3], numbers[3]*numbers[0]];
+//
+//let ab = Int.random(in: 0..<26);
+//print(ab);
+//
+//let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+//
+//let password = alphabet[Int.random(in: 0..<26)] + alphabet[Int.random(in: 0..<26)] + alphabet[Int.random(in: 0..<26)] + alphabet[Int.random(in: 0..<26)] + alphabet[Int.random(in: 0..<26)] + alphabet[Int.random(in: 0..<26)];
+//
+//print(password)
 
-var c = a;
-a = b;
-b = c;
 
-print("The value of a is \(a)");
-print("The value of b is \(b)");
+// functions
 
-// arrays
+func    greeting() {
+    print("Hello");
+};
 
-let numbers = [45, 50, 55, 60];
+func    greeting2() {
+    print("Hey");   
+};
 
-let computedNumbers = [numbers[0]*numbers[1], numbers[1]*numbers[2], numbers[2]*numbers[3], numbers[3]*numbers[0]];
 
-let ab = Int.random(in: 0..<26);
-print(ab);
 
-let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-let password = alphabet[Int.random(in: 0..<26)] + alphabet[Int.random(in: 0..<26)] + alphabet[Int.random(in: 0..<26)] + alphabet[Int.random(in: 0..<26)] + alphabet[Int.random(in: 0..<26)] + alphabet[Int.random(in: 0..<26)];
-    
-print(password)
+
+
+
+
+
 
